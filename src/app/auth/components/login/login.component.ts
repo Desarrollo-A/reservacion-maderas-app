5ba5ed73ba0b07d027b852e97c47bc8ea38323e6
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-05 12:20:06 -0500

    Validaciones en login * Se agregan validaciones en el componente de login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,53 +1,56 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { fadeInUp400ms } from "../../../shared/animations/fade-in-up.animation";
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { FormErrors } from "../../../shared/utils/form-error";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     fadeInUp400ms
   ]
 })
 export class LoginComponent implements OnInit {
-  form: UntypedFormGroup;
+  form: FormGroup;
+  formErrors: FormErrors;
 
   inputType = 'password';
+  tooltipPassword = 'Visualizar contraseña';
   visible = false;
 
   constructor(private router: Router,
-              private fb: UntypedFormBuilder,
-              private cd: ChangeDetectorRef,
-              private snackbar: MatSnackBar
+              private fb: FormBuilder
   ) {}
 
   ngOnInit() {
+    const data = JSON.parse(localStorage.getItem('credentials'));
+
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: [(data) ? data.email : '',
+        [Validators.required, Validators.email, Validators.maxLength(150)]],
+      password: [(data) ? data.password : '',
+        [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      rememberMe: [false]
     });
+
+    this.formErrors = new FormErrors(this.form);
   }
 
-  send() {
-    this.router.navigate(['/']);
-    this.snackbar.open('Lucky you! Looks like you didn\'t need a password or email address! For a real application we provide validators to prevent this. ;)', 'LOL THANKS', {
-      duration: 10000
-    });
+  send(): void {
+    //
   }
 
-  toggleVisibility() {
+  toggleVisibility(): void {
     if (this.visible) {
       this.inputType = 'password';
       this.visible = false;
-      this.cd.markForCheck();
+      this.tooltipPassword = 'Visualizar contraseña';
     } else {
       this.inputType = 'text';
       this.visible = true;
-      this.cd.markForCheck();
+      this.tooltipPassword = 'Ocultar contraseña';
     }
   }
 

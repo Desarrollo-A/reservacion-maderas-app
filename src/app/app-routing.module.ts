9d58e24a8320f79e456c0d3104c48ed099ce6897
ozuna16 <80979535+ozuna16@users.noreply.github.com>
2022-07-15 08:29:00 -0500

    RES-39

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { LoginGuard } from "./core/guards/login.guard";
 
 const routes: Routes = [
@@ -8,11 +9,14 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
     canActivateChild: [
       LoginGuard
-    ]
+    ],
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivateChild: [
+      AuthGuard
+    ]
   },
   {
     path: '**',
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -30,18 +30,38 @@ export class AuthService {
           if (data.rememberMe) {
             this.userSessionService.setCredentials(data);
           } else {
-            this.userSessionService.clearUser();
+            this.userSessionService.removeCredentials();
           }
           // TODO: falta poner la carga del menú
         })
       );
   }
 
-
   //Función para recuperación de contraseña
   forgotPassword(data: User): Observable<void> {
     const url = `${this.url}/restore-password`;
     return this.http.post<void>(url, data);   
   }
+
+  logout(): Observable<void> {
+    const url = `${this.url}/logout`;
+    return this.http.get<void>(url)
+    .pipe(
+      tap( () => {
+        // Se eliminan los datos de localStorage
+        this.userSessionService.removeToken();
+        this.userSessionService.clearUser();
+      })
+    );   
+  }
+
+  getUserSession(): Observable<UserSession> {
+    const url = `${this.url}/user`;
+    return this.http.get<UserSession>(url).pipe(
+      tap(user => this.userSessionService.setUser(user))
+    );
+
+  }
+
 
 }

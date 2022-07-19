0a9bf52f35a5b7dd16ef845c0957bc660036ca9e
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-19 17:03:14 -0500

    Diseño del formulario para apartar una sala de junta * Se agrega autocomplete y select al módulo de material * Se agrega material y reactive forms al módulo de request * Se agrega diseño del formulario de solicitud para sala de juntas

--- src/app/dashboard/request/pages/room/room.component.ts
+++ src/app/dashboard/request/pages/room/room.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { stagger60ms } from "../../../../shared/animations/stagger.animation";
+import { fadeInUp400ms } from "../../../../shared/animations/fade-in-up.animation";
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.scss']
+  styleUrls: ['./room.component.scss'],
+  animations: [
+    stagger60ms,
+    fadeInUp400ms
+  ]
 })
 export class RoomComponent implements OnInit {
 

80c583a5336e48056dad671ffdd103d3174a89f9
ozuna16 <80979535+ozuna16@users.noreply.github.com>
2022-07-21 15:16:42 -0500

    RES-48

--- src/app/dashboard/maintenance/maintenance-routing.module.ts
+++ src/app/dashboard/maintenance/maintenance-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './pages/car/car.component';
 import { RoomComponent } from "./pages/room/room.component";
 
 const routes: Routes = [
   { path: 'sala', component: RoomComponent, title: 'Manto. Salas de Junta' },
+  { path: 'auto', component: CarComponent, title: 'Manto. Automóvil' },
   { path: '**', redirectTo: 'sala' }
 ];
 

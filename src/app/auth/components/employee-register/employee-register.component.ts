57267e5474746f8f60abd88780ff3cb3ca8d624f
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-22 16:46:52 -0500

    Componente employee-register.component * Se crea validaciones para el input del # de empleado * Se agregan mensajes de error en el input * Se crea formulario en el ts

--- src/app/auth/components/employee-register/employee-register.component.ts
+++ src/app/auth/components/employee-register/employee-register.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { fadeInUp400ms } from "../../../shared/animations/fade-in-up.animation";
+import { FormErrors } from "../../../shared/utils/form-error";
 
 @Component({
   selector: 'app-employee-register',
@@ -13,19 +14,26 @@ export class EmployeeRegisterComponent implements OnInit {
   @Output()
   checkUser: EventEmitter<string> = new EventEmitter<string>();
 
+  form: FormGroup;
+  formErrors: FormErrors;
 
-  form = this.fb.group({
-    noEmployee: ['', Validators.required]
-  });
+  constructor(private router: Router,
+              private fb: FormBuilder) {}
 
-  constructor(
-    private router: Router,
-    private fb: FormBuilder
-  ) {}
+  ngOnInit() {
+    this.form = this.fb.group({
+      noEmployee: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]]
+    });
 
-  ngOnInit() {}
+    this.formErrors = new FormErrors(this.form);
+  }
 
-  send() {
+  check(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
     this.checkUser.emit(this.form.controls.noEmployee.value);
   }
 

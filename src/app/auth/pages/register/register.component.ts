839ac94608d1d301239a1c85d7046feb73bb9daa
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-22 17:05:09 -0500

    Componente de registro del usuario * Se crea método para verificar si el usuario está dado de baja, no existe, si no tiene correo corporativo o si está correcto * Se crea método para registrar el usuario en la bd y redirigirlo al login

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { ToastrService } from "ngx-toastr";
+import { Result } from "../../interfaces/response-user";
+import { UserModel } from "../../../dashboard/user/models/user.model";
+import { OfficeModel } from "../../../core/models/office.model";
+import { StateModel } from "../../../core/models/state.model";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,13 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  formHide = false;
+  formHide = true;
+  userData: UserModel;
 
-  constructor() {}
+  constructor(private authService: AuthService,
+              private toastrService: ToastrService,
+              private router: Router) {}
 
   ngOnInit() {}
 
-  checkUser(): void {
+  checkUser(noEmployee: string): void {
+    this.authService.checkEmployee(noEmployee).subscribe(({resultado, data}) => {
+      if (resultado === Result.NOT_EXIST) {
+        this.toastrService.error('No existe el registro', 'Error');
+      } else if (resultado === Result.USER_LOW) {
+        this.toastrService.error('Empleado dado de baja.', 'Error');
+      } else if (resultado === Result.ACTIVE_WITHOUT_EMAIL) {
+        this.toastrService.warning('No tiene correo corporativo. Favor de acercarse a Capital Humano',
+          'Empleado sin correo');
+      } else if (resultado === Result.ACTIVE_WITH_EMAIL) {
+        let state = new StateModel(data[0]);
+        const office = new OfficeModel(data[0]);
+        let user = new UserModel(data[0]);
+        user.isRecepcionist = (user.position.toUpperCase() === 'RECEPCIONISTA');
+        user.office = office;
+        user.office.state = state;
+        this.userData = user;
+
+        this.formHide = false;
+      }
+    });
+  }
+
+  backToCheckUser(): void {
     this.formHide = true;
+    this.userData = null;
+  }
+
+  register(user: UserModel): void {
+    this.authService.createUser(user).subscribe(() => {
+      this.toastrService.success('Usuario registrado.','Proceso exitoso');
+      this.router.navigateByUrl('/dashboard');
+    });
   }
 }

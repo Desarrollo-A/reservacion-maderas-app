637249d23e1b068be7700197b8d6250c960dbb1e
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-27 10:51:14 -0500

    Se crea servicio de lookup

---
+++ src/app/core/services/lookup.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+import { Observable } from "rxjs";
+import { Lookup } from "../interfaces/lookup";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LookupService {
+  private _baseUrl = 'lookups';
+
+  constructor(private http: HttpClient) {}
+
+  get url(): string {
+    return environment.baseUrl + environment.api + this._baseUrl;
+  }
+
+  findAllByType(type: number): Observable<Lookup[]> {
+    const url = `${this.url}/find-all-type/${type}`;
+    return this.http.get<Lookup[]>(url);
+  }
+}

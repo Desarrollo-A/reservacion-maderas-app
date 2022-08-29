3e136c639fd42723ad2463c41ad7cae934bf10f6
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-08-29 09:58:06 -0500

    Guard para permisos de vistas dependiendo el rol

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from "./dashboard.component";
 import { PageNotFoundComponent } from "../shared/components/page-not-found/page-not-found.component";
+import { PermissionGuard } from "../core/guards/permission.guard";
+import { NameRole } from "../core/enums/name-role";
 
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
@@ -15,38 +17,62 @@ const routes: Routes = [
       },
       {
         path: 'solicitud',//para URL
-        loadChildren: () => import('./request/request.module').then(m => m.RequestModule)
+        loadChildren: () => import('./request/request.module').then(m => m.RequestModule),
+        data: { roles: [ NameRole.APPLICANT ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'calendario',//para URL
         loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule),
         data: {
-          toolbarShadowEnabled: true
-        }
+          toolbarShadowEnabled: true,
+          roles: [ NameRole.APPLICANT, NameRole.RECEPCIONIST ]
+        },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'inventario',//para URL
-        loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)
+        loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule),
+        data: { roles: [ NameRole.RECEPCIONIST ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'reporte',//para URL
-        loadChildren: () => import('./report/report.module').then(m => m.ReportModule)
+        loadChildren: () => import('./report/report.module').then(m => m.ReportModule),
+        data: { roles: [ NameRole.RECEPCIONIST ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'historial',//para URL
-        loadChildren: () => import('./history/history.module').then(m => m.HistoryModule)
+        loadChildren: () => import('./history/history.module').then(m => m.HistoryModule),
+        data: { roles: [ NameRole.APPLICANT ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'solicitudes',//para URL
-        loadChildren: () => import('./history/history.module').then(m => m.HistoryModule)
+        loadChildren: () => import('./history/history.module').then(m => m.HistoryModule),
+        data: { roles: [ NameRole.RECEPCIONIST ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
         path: 'mantenimiento',
-        loadChildren: () => import('./maintenance/maintenance.module').then(m => m.MaintenanceModule)
+        loadChildren: () => import('./maintenance/maintenance.module').then(m => m.MaintenanceModule),
+        data: { roles: [ NameRole.RECEPCIONIST ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       {
-        path: 'usuario',
-        loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+        path: 'usuarios',
+        loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+        data: { roles: [ NameRole.ADMIN ] },
+        canActivate: [ PermissionGuard ],
+        canLoad: [ PermissionGuard ]
       },
       { path: '**', component: PageNotFoundComponent }
     ]

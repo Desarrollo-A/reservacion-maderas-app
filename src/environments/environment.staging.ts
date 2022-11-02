572e72440222e2e33fc329386b7fea8100898a23
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-11-02 13:15:08 -0600

    Configuración de un nuevo ambiente de staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://prueba.gphsis.com/recepcion-cm/',
+  api: 'api/v1/',
+  baseUrlCapital: 'https://prueba.gphsis.com/RHCV/index.php/WS/',
+  pusher: {
+    key: '5f57d786f2f227acfe1c',
+    cluster: 'us2'
+  }
+};

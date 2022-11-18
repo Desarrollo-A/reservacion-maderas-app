6edc55c7f5ebb087485a71558577d4a392168808
danteAldair <programador.analista18@ciudadmaderas.com>
2022-11-18 12:07:07 -0600

    Cambios Dante Aldair (Driver) /* *Se agrego vista para tabla de conductores con su debida informacion. *Dentro de los cambios se creo un componente, html, srvice, model y lookup.ts. *Se ligo la informacion con el desarrollo dentro del back que se realizo en esta misma semana (Driver). */

--- src/app/core/models/driver.model.ts
+++ src/app/core/models/driver.model.ts
@@ -1,3 +1,5 @@
+import { LabelButton } from "src/app/shared/interfaces/label-button";
+import { StatusDriverLookup } from "../enums/lookups/status-driver.lookup";
 import { Lookup } from "../interfaces/lookup";
 
 export class DriverModel {
@@ -15,12 +17,26 @@ export class DriverModel {
     constructor(driver) {
         this.id = driver.id;
         this.noEmployee = driver.noEmployee;
-        this.fullName = driver.noEmployee;
+        this.fullName = driver.fullName;
         this.email = driver.email;
         this.personalPhone = driver.personalPhone;
         this.officePhone = driver.officePhone;
         this.officeId = driver.officeId;
         this.statusId = driver.statusId;
         this.status = driver.status;
+    }
+
+    get statusName(): string{
+        return this.status.name;
+    }
+
+    get statusLabel(): LabelButton{
+        if(this.status.code === StatusDriverLookup[StatusDriverLookup.ACTIVE]){
+            return {text: this.statusName, textClass: 'text-green', bgClass: 'bg-green-light'};
+        }else if(this.status.code === StatusDriverLookup[StatusDriverLookup.INACTIVE]){
+            return {text: this.statusName, textClass: 'text-gray', bgClass: 'bg-gray-light'};
+        }else if(this.status.code === StatusDriverLookup[StatusDriverLookup.DOWN]){
+            return {text: this.statusName, textClass: 'text-red', bgClass: 'bg-red-light'};
+        }
     }
 }

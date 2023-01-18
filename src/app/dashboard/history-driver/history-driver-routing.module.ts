9a06d979abf9e28243fa88aeb047d4f7d6114109
danteAldair <programador.analista18@ciudadmaderas.com>
2023-01-18 10:55:34 -0600

    Creacion de nueva vista para menu solicitudes con rol DRIVER y modificacion de titulo en la vista de paqueteria dentro del mismo menu. /* *Se creo un nuevo componente dentro de la carpeta history-driver el cual mostrara una tabla con la informacion de los choferes asignados a una solicitud. *Se modifico el titulo de la vista "paqueteria" de Historial a Solicitudes". *Modificacion dentro del modelo request-driver-view, se agrego un dato "driverId". *Se agrego ruta de Pagina no encontrada en el routing de history-driver. *Se agrego nueva funcion en request-driver.service parael llamado del endPoint que trae informacion de la vista request_driver_view. */

--- src/app/dashboard/history-driver/history-driver-routing.module.ts
+++ src/app/dashboard/history-driver/history-driver-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from 'src/app/shared/components/page-not-found/page-not-found.component';
+import { DriverComponent } from './pages/driver/driver.component';
 import { PackageComponent } from './pages/package/package.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'paqueteria', pathMatch: 'full'},
   {path: 'paqueteria', component: PackageComponent, title: 'Paquetería'},
+  {path: 'conductor', component: DriverComponent, title: 'Chofer'},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({

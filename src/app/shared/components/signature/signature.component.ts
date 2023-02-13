43eb9a331d5fcf64ea43d614f2b2e298a4fe54a8
danteAldair <programador.analista18@ciudadmaderas.com>
2023-02-13 08:39:40 -0600

    Cambio de tamaño en firma electrónica. /* *Se intentó hacer un cambio de tamaño a la firma electrónica mediante una operación donde se obtiene una escala según el cambio de tamaño del canvas, estos puntos se guardan en otro arreglo, ya que al momento de que se ejecuta el evento touchstart y onMouseDown la primer matriz que usamos para firma "original" se limpia y así no nos sirve para la creación de una "nueva" firma (esto en caso en que la firma se haga en múltiples pulsaciones de pantalla o clic en mouse). *De igual forma se detectó que habría que tomar en cuenta en él nuevo arreglo se indicará cuando es que el usuario pulso varias veces la pantalla o el clic del mouse para su firma, esto para tomar en cuenta en que momento habría que terminar la línea continua de la firma y donde volver a pintar la siguiente línea. *Otra opción para el renderizado de esto es que al momento de pulsar clic en el botón de "ok" este se guarde como imagen temporalmente (si es posible) y posteriormente cuando cambie de tamaño el canvas pegar dicha imagen en el mismo pero renderizado con la función drawImage la cual pega la imagen deseada en canvas manipulando las medidas de esta misma, solo tomar en cuenta en que momento se eliminaría dicha imagen y en que momento sé mandaría a llamar. */

--- src/app/shared/components/signature/signature.component.ts
+++ src/app/shared/components/signature/signature.component.ts
@@ -13,11 +13,28 @@ export class SignatureComponent implements AfterViewInit {
   private context : CanvasRenderingContext2D;
   private points: Array<any> = [];
 
+  signaturePoints: Array<any> = [];
+  pointsNumber = 1;
+
   isSigning: boolean = false;
   width: number;
   height: number;
   isAvailable: boolean;
   canvasDownload = null;
+
+  @HostListener('document:click', ['$event'])
+  OnClick = (event) => {
+    
+    if(event.target.id === 'canvas'){
+      console.log(event);
+      
+    }else{
+    }
+    // if(event.target.id === 'canvas'){
+    //   console.log(this.canvasRef.nativeElement.getBoundingClientRect());
+    //   console.log(event);
+    // }
+  }
 
   @HostListener('window:resize', ['$event'])
   OnRezise = () => {
@@ -27,6 +44,11 @@ export class SignatureComponent implements AfterViewInit {
   @HostListener('document:mousemove', ['$event'])
   onMouseMove = (e: MouseEvent) => {
     if (e.target['id'] === 'canvas' && (this.isAvailable) && (this.isSigning)) {
+      let position = {
+        x: e.offsetX,
+        y: e.offsetY
+      };
+      this.signaturePoints.push(position);
       this.canvasDownload = e.target;
       this.write(e);
     }
@@ -45,6 +67,7 @@ export class SignatureComponent implements AfterViewInit {
     if(e.target['id'] === 'canvas'){
       this.points = [];
       this.isAvailable = true;
+      this.pointsNumber = this.pointsNumber + 1;
     }
   }
 
@@ -53,6 +76,7 @@ export class SignatureComponent implements AfterViewInit {
     if(e.target['id'] === 'canvas'){
       this.points = [];
       this.isAvailable = true;
+      this.pointsNumber = this.pointsNumber + 1;
     }
   }
 
@@ -80,10 +104,13 @@ export class SignatureComponent implements AfterViewInit {
     if(canvasParentSize.width !== canvasSize.width + 32){
       this.renderResize(false)
     }
+    this.width = Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0]);
+    this.height = Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0]);
   }
 
   clearZone(): void {
-    this.points = []
+    this.points = [];
+    this.signaturePoints = [];
     const canvasSize = this.canvasRef.nativeElement.getBoundingClientRect();
     this.context.clearRect(0, 0, canvasSize.width, canvasSize.height);
   }
@@ -96,6 +123,89 @@ export class SignatureComponent implements AfterViewInit {
   isOk(){
     this.layaoutService.enableScrollBarOnSideNavContent();
     this.isSigning = false;
+  }
+
+  changeSize(){
+    const canvasEl = this.canvasRef.nativeElement;
+    let pointPreX = 0;
+    let pointPreY = 0;
+    let pointPosX = 0;
+    let pointPosY = 0;
+    let escaleX = 0;
+    let escaleY = 0;
+    
+    let contextDowload = canvasEl.getContext('2d');
+    this.context.clearRect(0, 0, Number((getComputedStyle(this.canvasRef.nativeElement).width)), Number((getComputedStyle(this.canvasRef.nativeElement).height)));
+    contextDowload.lineWidth = 2;
+    contextDowload.lineCap = 'round';
+    contextDowload.fillStyle = "white";
+    contextDowload.fillRect(0, 0, canvasEl.width, canvasEl.height);
+    contextDowload.beginPath();
+    // console.log(this.signaturePoints);
+    // console.log(differenceX);
+    // console.log(differenceY);
+    for (let index = 0; index < this.signaturePoints.length-1; index++) {
+
+      if(this.width === Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0])){
+        pointPreX = this.signaturePoints[index].x;
+        pointPosX = this.signaturePoints[index+1].x;
+      }
+      else if (this.width < Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0])) {
+        escaleX = Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0]) / this.width;      
+        pointPreX = this.signaturePoints[index].x * escaleX;
+        pointPosX = this.signaturePoints[index+1].x * escaleX;
+      }else if(this.width > Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0])){
+        escaleX = this.width / Number((getComputedStyle(this.canvasRef.nativeElement).width).split("px")[0]);
+        pointPreX = this.signaturePoints[index].x / escaleX;
+        pointPosX = this.signaturePoints[index+1].x / escaleX;
+      }
+
+      if(this.height === Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0])){
+        pointPreY = this.signaturePoints[index].y;
+        pointPosY = this.signaturePoints[index+1].y;
+      }
+      else if (this.height < Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0])) {
+        escaleY = Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0]) / this.width;      
+        pointPreY = this.signaturePoints[index].y * escaleY;
+        pointPosY = this.signaturePoints[index+1].y * escaleY;
+      }else if(this.height > Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0])){
+        escaleY = this.height / Number((getComputedStyle(this.canvasRef.nativeElement).height).split("px")[0]);
+        pointPreY = this.signaturePoints[index].y / escaleY;
+        pointPosY = this.signaturePoints[index+1].y / escaleY;
+      }
+      // if(differenceX < 0){
+      //   pointPreX = this.signaturePoints[index].x + differenceX;
+      //   pointPosX = this.signaturePoints[index-1].x + differenceX;
+      // }else{
+      //   pointPreX = this.signaturePoints[index].x - differenceX;
+      //   pointPosX = this.signaturePoints[index-1].x - differenceX;
+      // }
+      // if (differenceY < 0) {
+      //   pointPreY = this.signaturePoints[index].y - differenceY;
+      //   pointPosY = this.signaturePoints[index-1].y - differenceY;
+      // }else{
+      //   pointPreY = this.signaturePoints[index].y + differenceY;
+      //   pointPosY = this.signaturePoints[index-1].y + differenceY;
+      // }
+      // console.log(pointPreX, '<->', pointPosX);
+      // console.log(pointPreY, '<->', pointPosY);
+      // pointPreX = this.signaturePoints[index].x;
+      // pointPosX = this.signaturePoints[index-1].x;
+      // pointPreY = this.signaturePoints[index].y;
+      // pointPosY = this.signaturePoints[index-1].y;
+      
+      contextDowload.moveTo(pointPosX, pointPosY);
+      contextDowload.lineTo(pointPreX, pointPreY );
+      contextDowload.stroke();
+    }
+    // this.signaturePoints.reverse()
+    //   .forEach(function(item, key, arr){
+    //     if(key < arr.length-1){
+    //       contextDowload.moveTo(arr[key].x, arr[key].y );
+    //       contextDowload.lineTo(arr[key+1].x, arr[key+1].y);
+    //       contextDowload.stroke();
+    //     }
+    // });
   }
 
   private setCanvasSize(){
@@ -116,13 +226,14 @@ export class SignatureComponent implements AfterViewInit {
       : canvasEl.width = (canvasParentSize.width - 32);
     if(window.innerWidth < window.innerHeight){
       isResponsive
-        ? canvasEl.height = this.height
+        ? canvasEl.height = this.height - 8
         : canvasEl.height = canvasEl.width * 0.4;
     }else{
       isResponsive
-        ? canvasEl.height = this.height
+        ? canvasEl.height = this.height - 8
         : canvasEl.height = window.innerHeight * 0.5;
     }
+
     this.context = canvasEl.getContext('2d');
     this.context.lineWidth = 3;
     this.context.lineCap = 'round';
@@ -135,12 +246,9 @@ export class SignatureComponent implements AfterViewInit {
     const canvasEl = this.canvasRef.nativeElement;
     let canvasParentSize = this.canvasRef.nativeElement.parentElement.parentElement.getBoundingClientRect();
     this.context = canvasEl.getContext('2d');
-
-    this.height = canvasParentSize.height;
-    this.width = canvasParentSize.width;
-
+    
     canvasEl.width = (canvasParentSize.width) / 2;
-    canvasEl.height = canvasParentSize.height;
+    canvasEl.height = canvasEl.width * 0.5;
 
     this.context.lineWidth = 3;
     this.context.lineCap = 'round';
@@ -161,6 +269,7 @@ export class SignatureComponent implements AfterViewInit {
 
   private writeSingle(prePos): void {
     this.points.push(prePos);
+    this.signaturePoints.push(prePos);
     if (this.points.length > 1) {
       const prevPos = this.points[this.points.length - 1]
       const currentPos = this.points[this.points.length - 2]

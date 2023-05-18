bb4c2bdfdbf9ffa87793839c1a2fc1d7a09a1b75
jcoronelg <programador.analista24@ciudadmaderas.com>
2023-05-18 09:56:25 -0600

    Se elimina endpoint para mandar propuesta de solicitud de paquetería porque ya no existirá esa opción

--- src/app/dashboard/history/interfaces/proposal-package-request.ts
+++
@@ -1,9 +0,0 @@
-export interface ProposalPackageRequest {
-  requestId: number;
-  startDate: string;
-  isDriverSelected: boolean;
-  packageId: number;
-  carId?: number;
-  driverId?: number;
-  endDate?: string;
-}
